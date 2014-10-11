c2c3e5362ab602a898f386e7b8a14c19a380ad6f
drenfr01 <duncanrenfrow@gmail.com>
2014-10-11 18:31:43 -0400

    adding vehicle collection

---
+++ whip/collection/vehicle.js
@@ -0,0 +1,41 @@
+Vehicles = new Meteor.Collection('vehicles');
+
+Vehicles.attachSchema(new SimpleSchema({
+  make: {
+    type: String,
+    label: 'Vehicle Make'
+  },
+  model: {
+    type: String,
+    label: 'Vehicle Model'
+  }, 
+  color: {
+    type: String,
+    label: 'Vehicle Color'
+  }, 
+  mileage: {
+    type: Number,
+    label: 'Vehicle Mileage'
+  },
+  year: {
+    type: Number,
+    label: 'Vehicle Mileage'
+  },
+  vehicleHistory: {
+    type: [Object],
+    label: 'Vehicle History',
+    optional: true
+  }
+}));
+
+Vehicles.allow({
+  insert: function() {
+    return true;
+  },  
+  update: function() {
+    return true;
+  },  
+  remove: function() {
+    return true;
+  },  
+});
